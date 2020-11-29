d8cad306b0520f7bd42caf91e2999871cbb3150c
leuhk <44567291+leuhk@users.noreply.github.com>
2020-11-29 16:46:30 +0800

    web socket config
    set up a web socket session listen to user's data

--- index.js
+++ index.js
@@ -1,38 +1,54 @@
 const axios = require("axios");
 const crypto = require("crypto");
-const fs = require('fs')
-    /**
-     * To DO
-     * 1. set up api to POST /api/v3/order
-     * 2. set up websocket to listen to excutionReport 
-     * 3. set up terminal user's input for recvWindow
-     */
+const WebSocket = require('ws')
+const readline = require("readline");
+/**
+ * To DO
+ * 1. set up api to POST /api/v3/order
+ * 2. set up websocket to listen to excutionReport 
+ * 3. set up terminal user's input for recvWindow
+ */
 
 async function create_order() {
-    const pk = fs.readFileSync('./test-prv-key.pem', 'utf8')
 
-
-    const querystring = 'symbol=LTCBTC&side=BUY&type=LIMIT&timeInForce=GTC&quantity=1&price=0.003&recvWindow=5000&timestamp=' + Date.now()
+    const querystring = 'symbol=LTCBTC&side=SELL&type=LIMIT&timeInForce=GTC&quantity=0.1&price=0.004&recvWindow=5000&timestamp=' + Date.now()
 
     const hash = crypto.createHmac("sha256", 'wbA3hCx9yztZRMxuIhqsl9242FtZwkhAaut4W2XORtpy9rHFou9Eefn3VmxUJpGC')
         .update(querystring.toString())
         .digest('hex')
-    console.log({ hash: hash })
     try {
-        const result = await axios.request({
-            url: "https://testnet.binance.vision/api/v3/order?" + querystring + "&signature=" + hash,
+        const listen = await axios.request({
+            url: "https://testnet.binance.vision/api/v3/userDataStream",
             method: "POST",
             headers: {
                 "X-MBX-APIKEY": "3mlL812V0wRDUjrU2T6y9Y6h40hHVtjej57PEYDfu4QhGwAzHOnERjhYuL2ZGowM"
             }
         })
-        console.log(result)
-        return result;
+        console.log(listen.data)
+        const url = "wss://testnet.binance.vision/ws/" + listen.data.listenKey
+        const ws = new WebSocket(url);
+        ws.on('open', async function open() {
+            const result = await axios.request({
+                url: "https://testnet.binance.vision/api/v3/order?" + querystring + "&signature=" + hash,
+                method: "POST",
+                headers: {
+                    "X-MBX-APIKEY": "3mlL812V0wRDUjrU2T6y9Y6h40hHVtjej57PEYDfu4QhGwAzHOnERjhYuL2ZGowM"
+                }
+            })
+            console.log(result.data)
+        });
+
+        ws.on('message', function incoming(data) {
+            console.log(data);
+        });
+
+
     } catch (err) {
         console.log(err)
     }
 
+
 }
 
 
-create_order();
+// create_order();
